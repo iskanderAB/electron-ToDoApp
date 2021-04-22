7dae31fe2597b78f2467a6441bc78cc9108ecc7e
iskanderAB <iskanderabasi@gmail.com>
2021-04-22 04:50:26 +0100

    complete training

--- app.js
+++ app.js
@@ -1,54 +1,53 @@
 const electron = require('electron');
 
-const { app , BrowserWindow , Menu } = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 let mainWindow;
 let addToDoWindow;
 
-app.on('ready',()=>{
+app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        nodeIntegration :true,
-        contextIsolation :false
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     });
     mainWindow.loadURL(`file://${__dirname}/public/main.html`);
-    mainWindow.on('closed',()=> {
+    mainWindow.on('closed', () => {
         app.quit();
     });
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
 });
-
 const creatAddNewTodo = () => {
     addToDoWindow = new BrowserWindow({
         width: 300,
         height: 200,
-        title: 'Add New ToDo',
-        isMenuBarAutoHide :true,
-        nodeIntegration :true,
-        contextIsolation :false
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     });
     addToDoWindow.loadURL(`file://${__dirname}/public/add.html`);
-    addToDoWindow.removeMenu();
+    addToDoWindow.on('closed',()=> addToDoWindow = null);
 };
-const menuTemplate =[
+ipcMain.on('todo:add', (e, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addToDoWindow.close();
+});
+const menuTemplate = [
     {
-        label : 'File',
-        submenu :[
+        label: 'File',
+        submenu: [
             {
-                label : 'New ToDo',
-                click: ()=> {
+                label: 'New ToDo',
+                accelerator: 'Ctrl+N' ,
+                click: () => {
                     creatAddNewTodo();
                 }
             },
             {
                 label: 'Quit',
-                accelerator : process.platform === 'darwin' ? 'command+Q': 'ctrl+Q',
-                // accelerator : ()=> {
-                //   if (process.platform === 'darwin') {
-                //       return 'Command+Q';
-                //   }else {
-                //       return 'ctrl+Q';
-                //   }
-                // },
+                accelerator: process.platform === 'darwin' ? 'command+Q' : 'ctrl+Q',
                 click: () => {
                     app.quit();
                 }
@@ -56,26 +55,29 @@ const menuTemplate =[
         ]
     }
 ];
-
- //if u have osx system !
- if(process.platform === 'darwin'){
-     menuTemplate.unshift({
-         label: ""
-     });
- }
-// if mode == developer
-if (process.env.NODE_ENV !== 'prod'){
+/*if u have osx system ! */
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({
+        label: ""
+    });
+}
+/*if mode == developer */
+console.log(app.isPackaged);
+if (!app.isPackaged) {
     menuTemplate.push({
-       label: 'View',
-       submenu: [
-           {
-               label: 'Toggle developer Tools',
-               accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I' ,
-               click: (item , focusedWindow)=> {
-                   focusedWindow.toggleDevTools();
-               }
-           }
-       ]
+        label: 'View',
+        submenu: [
+            {
+                role : 'reload'
+            },
+            {
+                label: 'Toggle developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click: (item, focusedWindow) => {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
     });
 }
 
